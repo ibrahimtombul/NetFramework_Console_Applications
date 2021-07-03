9cac1c96bf7209a22e4d55953a0037a02e6bad9d
ibrahim tombul <ibrahim_tombul@hotmail.com>
2021-07-03 19:08:21 +0300

    Soru-2

---
+++ Soru-2/Program.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Soru_2
+{
+	class Program
+	{
+		static void Main(string[] args)
+		{
+			//Klavyeden 10K kelimesi girilene kadar kelimeleri bir değişkene atıp ekrana yazdıran programın kodlarını yazınız.
+			string kelime = "";
+			string sonuc = "";
+			do
+			{
+				Console.WriteLine("Tekrar");
+				kelime = Console.ReadLine();
+				sonuc += kelime + " - ";
+			} while (kelime != "10K");
+
+			Console.WriteLine("Sonuc: " + sonuc);
+		}
+	}
+}
